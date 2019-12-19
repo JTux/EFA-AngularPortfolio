eb772fae4ded3a7356e73bdae2f7f1e6e5ca6d1c
Joshua Tucker <thejltucker@gmail.com>
2019-12-18 22:48:51 -0500

    Quick style changes

--- src/app/components/navigation/sidebar/subtitle/subtitle.component.ts
+++ src/app/components/navigation/sidebar/subtitle/subtitle.component.ts
@@ -14,6 +14,7 @@ export class SubtitleComponent implements OnInit {
   }
 
   ngOnInit() {
+    this.subtitleArray.forEach((subtitle) => subtitle.text = subtitle.text.toUpperCase());
     this.shuffleArray(this.subtitleArray);
   }
 
