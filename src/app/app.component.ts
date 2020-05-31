e13e6c4a80d41d1035dcd27a40e90f7d9f7122dd
Joshua Tucker <thejltucker@gmail.com>
2020-05-31 16:30:55 -0400

    General information changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,30 @@
-import { Component, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TuckerEFAPortfolio';
 
-  @ViewChild('bodyContent', {static: false}) scrollContent: ElementRef;
+  @ViewChild('bodyContent', { static: false }) scrollContent: ElementRef;
 
   constructor() { }
-  
+
   onActivate() {
     let body = this.scrollContent.nativeElement;
     body.style.scrollBehavior = 'auto';
     body.scrollTop = 0;
     body.style.scrollBehavior = 'smooth';
+  }
+
+  ngOnInit() {
+    if (environment.production) {
+      if (location.protocol === 'http:') {
+        window.location.href = location.href.replace('http', 'https');
+      }
+    }
   }
 }
