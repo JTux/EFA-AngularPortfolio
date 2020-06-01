899d6148f69af3b07ab441296e0bb58fc18ac689
Joshua Tucker <the1jtux@gmail.com>
2020-06-01 16:00:47 -0400

    Random text filled in finally

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: window.location.hostname.includes('localhost:4200')
+  production: !window.location.hostname.includes('localhost:4200')
 };
 
 /*
